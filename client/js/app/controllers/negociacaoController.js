becdf139bce3258c1b8e77417dbf5ba6ef78acda
beduardo.dev <beduardo.dev@gmail.com>
2020-12-16 21:15:06 -0300

    Buscando valores dos inputs e guardandos nas variáveis

--- client/js/app/controllers/negociacaoController.js
+++ client/js/app/controllers/negociacaoController.js
@@ -1,5 +1,12 @@
 class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
+        let $ = document.querySelector.bind(document);
+
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+
     }
 }
