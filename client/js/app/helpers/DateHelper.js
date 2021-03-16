99ef9c0a8254b531510bc67fff383501e146ed8c
BEduardo <beduardo.dev@gmail.com>
2021-03-16 20:07:28 -0300

    regex utilizada para validar a data estava incorreta

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -6,14 +6,14 @@ class DateHelper {
 
     static textoParaData(texto) {
 
-        if (!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error('Data está fora do formato!');
+        if (/^\d{4}-\d{2}-\d{2}$/.test(texto)) throw new Error('Data está fora do formato!');
 
         return new Date(texto.split('-'));
     }
 
     static dataParaTexto(data) {
         return `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}`;
-        
+
 
     }
 }
