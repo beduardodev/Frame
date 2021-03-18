da0c33236686361fcec36644026497b061abfeed
BEduardo <beduardo.dev@gmail.com>
2021-03-18 20:33:23 -0300

    Criada funcao para criacao do grid de exibicao, e adicionar itens. Tambem foi criado um roda pe com o somatorio dos itens adicionados

--- client/js/app/controllers/negociacaoController.js
+++ client/js/app/controllers/negociacaoController.js
@@ -1,47 +1,39 @@
 class NegociacaoController {
-    constructor() {
-        let $ = document.querySelector.bind(document);
 
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+
     }
 
     adiciona(event) {
+
         event.preventDefault();
-
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
-
-        console.log(this._listaNegociacoes.negociacoes);
-
-        let listaDeNomes1 = ['Flávio', 'Rogers', 'Júlia'];
-        let listaDeNomes2 = ['Vieira', 'Fernanda', 'Gerson'];
-        this._exibeNoConsole(listaDeNomes1.concat(listaDeNomes2));
     }
 
     _criaNegociacao() {
 
-        new Negociacao(
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        );
+            this._inputValor.value);
     }
 
     _limpaFormulario() {
 
-        this._inputData.value = "";
+        this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
-
         this._inputData.focus();
     }
-
-    _exibeNoConsole(lista) {
-        lista.forEach(item => console.log(item));
-    }
-
-
 }
